330d86ce15e11a873cf9c85f49ed42bf36a1e0ee
allan.pirillis <allan.p94@gmail.com>
2021-07-28 17:34:56 -0500

    deleted some code

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,15 +1,6 @@
 import React, { useState } from "react";
-import { Document, Page } from "react-pdf";
-import File from "../../img/APResumeKoumbaros9.pdf";
 
 const Resume = () => {
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
-
   return (
     <>
       <section className="hero is-small notification">
